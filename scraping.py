3a66f66946327632cb7b6c7387fdf106c3857e69
cesreve <cesreve@gmail.com>
2020-11-01 10:44:34 +0100

    Ajout caracteristiques livre"

--- scraping.py
+++ scraping.py
@@ -30,13 +30,27 @@ for book in books.find_all('li'):
     for _ in head.find_all('li'):
         j += 1
         if j ==3:
-            print(_.text)
+            category = _.text
+            print(category)
 
-
+    # title
+    title = soup_book.find('div', class_='col-sm-6 product_main').h1.text
+    #print(title)
 
     # table
     result = soup_book.find('table', class_='table table-striped')
-    #print((result.find('tr').th).text, (result.find('tr').td).text)
+    # for d in result.find_all('td'):
+    #    print(d.text[:])
+    for h, d in zip(result.find_all('th'), result.find_all('td')):
+        if h.text[0:3] == 'UPC':
+            print(d.text)
+        else:
+            print(d.text[1:])
+
+    # image
+    lien_img = soup_book.find('div', class_='item active')
+    print('http://books.toscrape.com/'+lien_img.img['src'])
+
 
     if i >= 1:
         break
